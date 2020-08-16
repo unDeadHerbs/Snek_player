6eaa9454ef9ef5bacd9aa6358796dfe9ad9c1f4e
Murray Fordyce <unDeadHerbs@gmail.com>
2020-08-16 11:35:26 -0500

    Seperated body from graphics, that was causing a large slowdown

--- snek_game.cpp
+++ snek_game.cpp
@@ -42,7 +42,8 @@ Snek::Snek() {
   size = cio.size();
   size.first -= 2;
   size.second -= 2;
-  body.push_back({{size.first / 2, size.second / 2},'s'}); // Start in the center.
+  body.push_back({size.first / 2, size.second / 2});
+  body_graphics.push_back('s'); // Start in the center.
 
   food = rand_point({1, 1}, {size.first - 1, size.second - 1});
 
@@ -70,18 +71,18 @@ void Snek::updateDisplay() const {
   if (old_end!=Point{0,0})
     cio[old_end.first][old_end.second] = ' ';
   if (body.size() > 1){
-    cio[body[1].first.first][body[1].first.second] =
-        body[1].second; // In case we haven't moved and need to not delete that.
-    cio[body.back().first.first][body.back().first.second] =
-      body.back().second; // In case we haven't moved and need to not delete that.
+    cio[body[1].first][body[1].second] =
+        body_graphics[1]; // In case we haven't moved and need to not delete that.
+    cio[body.back().first][body.back().second] =
+      body_graphics.back(); // In case we haven't moved and need to not delete that.
   }
   if (alive)
-    cio[body.front().first.first][body.front().first.second] = body.front().second;
+    cio[body.front().first][body.front().second] = body_graphics.front();
   else
-    cio[body.front().first.first][body.front().first.second] = '!';
+    cio[body.front().first][body.front().second] = '!';
 
   cio[food.first][food.second] = 'a'; // Draw food over body in case of glitch
-  old_end = body.back().first;
+  old_end = body.back();
   cio << std::flush;
   usleep(sleep_time);
 }
@@ -94,78 +95,81 @@ bool Snek::move(Direction movement_input) {
   case none:
     return true;
   case up:
-    body.insert(body.begin(), {{body[0].first.first - 1, body[0].first.second},'^'});
-    if(body.size()>1)
-      switch(body[1].second){
+    body.insert(body.begin(), {body[0].first - 1, body[0].second});
+    body_graphics.insert(body_graphics.begin(), '^');
+    if(body_graphics.size()>1)
+      switch(body_graphics[1]){
       case '^':
-	body[1].second='|';
+	body_graphics[1]='|';
 	break;
       case '>':
-	body[1].second='/';
+	body_graphics[1]='/';
 	break;
       case 'v':
 	break;
       case '<':
-	body[1].second='\\';
+	body_graphics[1]='\\';
 	break;
       }
     break;
   case right:
-    body.insert(body.begin(), {{body[0].first.first, body[0].first.second + 1},'>'});
-    if(body.size()>1)
-      switch(body[1].second){
+    body.insert(body.begin(), {body[0].first, body[0].second + 1});
+    body_graphics.insert(body_graphics.begin(), '>');
+    if(body_graphics.size()>1)
+      switch(body_graphics[1]){
       case '^':
-	body[1].second='/';
+	body_graphics[1]='/';
 	break;
       case '>':
-	body[1].second='-';
+	body_graphics[1]='-';
 	break;
       case 'v':
-	body[1].second='\\';
+	body_graphics[1]='\\';
 	break;
       case '<':
 	break;
       }
     break;
   case down:
-    body.insert(body.begin(), {{body[0].first.first + 1, body[0].first.second},'v'});
-    if(body.size()>1)
-      switch(body[1].second){
+    body.insert(body.begin(), {body[0].first + 1, body[0].second});
+    body_graphics.insert(body_graphics.begin(), 'v');
+    if(body_graphics.size()>1)
+      switch(body_graphics[1]){
       case '^':
 	break;
       case '>':
-	body[1].second='\\';
+	body_graphics[1]='\\';
 	break;
       case 'v':
-	body[1].second='|';
+	body_graphics[1]='|';
 	break;
       case '<':
-	body[1].second='/';
+	body_graphics[1]='/';
 	break;
       }
     break;
   case left:
-    body.insert(body.begin(), {{body[0].first.first, body[0].first.second - 1},'<'});
-    if(body.size()>1)
-      switch(body[1].second){
+    body.insert(body.begin(), {body[0].first, body[0].second - 1});
+    body_graphics.insert(body_graphics.begin(),'<');
+    if(body_graphics.size()>1)
+      switch(body_graphics[1]){
       case '^':
-	body[1].second='\\';
+	body_graphics[1]='\\';
 	break;
       case '>':
 	break;
       case 'v':
-	body[1].second='/';
+	body_graphics[1]='/';
 	break;
       case '<':
-	body[1].second='-';
+	body_graphics[1]='-';
 	break;
       }
     break;
   }
-  if (body[0].first == food){
-    auto b=Body();
-    while (std::find(b.begin(), b.end(), food) !=
-           b.end()) { // Generate a new food.
+  if (body[0] == food){
+    while (std::find(body.begin(), body.end(), food) !=
+           body.end()) { // Generate a new food.
       food = rand_point({1, 1}, {size.first - 1, size.second - 1});
       /*food.first++; // TODO: random, this is just lexicographic
       food.second+=food.first==size.first;
@@ -175,14 +179,15 @@ bool Snek::move(Direction movement_input) {
       if(food.second==0)food.second++;*/
     }
   }
-  else
+  else{
     body.pop_back(); // Don't get longer by one.
-  if (body[0].first.first == 0 || body[0].first.first == size.first + 1 ||
-      body[0].first.second == 0 || body[0].first.second == size.second + 1)
+    body_graphics.pop_back();
+  }
+  if (body[0].first == 0 || body[0].first == size.first + 1 ||
+      body[0].second == 0 || body[0].second == size.second + 1)
     alive = false;
-  auto b=Body();
-  if (std::find(b.begin() + 1, b.end(), b[0]) !=
-      b.end()) // If the new head is in the body, die.
+  if (std::find(body.begin() + 1, body.end(), body[0]) !=
+      body.end()) // If the new head is in the body, die.
     alive = false;
   return true;
 }
--- snek_game.hpp
+++ snek_game.hpp
@@ -8,17 +8,13 @@ typedef std::pair<uint, uint> Point;
 int distance(Point, Point, int);
 class Snek {
   Point size;
-  std::vector<std::pair<Point,char>> body; // [0] is head
+  std::vector<Point> body; // [0] is head
+  std::vector<char> body_graphics; // [0] is head
   Point food;
 
 public:
   auto Size() const{return size;}
-  auto Body() const {
-    std::vector<Point> b;
-    for(auto seg:body)
-      b.push_back(seg.first);
-    return b;
-  }
+  auto Body() const { return body; }
   auto Food() const { return food; }
 
 public:
